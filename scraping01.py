e83d7ea57c2b53a12bfa39ed458278fcc8747c36
YMP <ympnov22@gmail.com>
2017-02-09 05:23:07 +0000

    scraping00.py

---
+++ scraping01.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import urllib2
+
+url = "http://news.yahoo.co.jp"
+html = urllib2.urlopen(url)
+
+#print(html.read())
+
+#-----------------------------------------------
+
+from bs4 import BeautifulSoup
+
+soup = BeautifulSoup(html, "html.parser")
+
+print(soup.findAll())
